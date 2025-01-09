6fe8ee99d0dd0dcc37b0a8e742de506fe050f628
kamil079 <kamil@nextgm.ca>
2025-01-09 12:12:50 +0100

    more about props and components

--- src/components/CoreConcepts.jsx
+++ src/components/CoreConcepts.jsx
@@ -2,14 +2,12 @@ import CoreConcept from './CoreConcept.jsx';
 
 import { CORE_CONCEPTS } from '../data.js';
 
-export default function Main() {
+export default function CoreConcepts() {
     return (
             <section id="core-concepts">
                 <h2>Core Concepts</h2>
                 <ul>
-
                     {CORE_CONCEPTS.map((conceptItem) => <CoreConcept key={conceptItem.title} {...conceptItem} /> )}
-
                     {/* <CoreConcept 
                         title={CORE_CONCEPTS[0].title}
                         description={CORE_CONCEPTS[0].description} 
